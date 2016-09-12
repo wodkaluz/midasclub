f18d3738122fdf9d068a9fafeb27136ceb31a826
wodkaluz <git@lucasluz.com>
2016-09-12 10:06:11 -0300

    Add files via upload

---
+++ imagem.php
@@ -0,0 +1,17 @@
+<DOCTYPE! html>
+
+    <?php
+    function imagem($src, $alt, $title, $width, $height) {
+        $img = "<img src=$src alt=$alt title=$title width=$width height=$height>";
+        return $img;
+    }
+    ?>
+    
+    
+    
+<html>
+    <head>
+    </head>
+    <body>
+    </body>
+</html>
